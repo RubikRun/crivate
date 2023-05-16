172f08a140179b01afec81570ddb2c04b4f95dbf
Boris Dimitrov <borisd2000@gmail.com>
2023-05-16 13:16:07 +0300

    in MathUtils add a function for calculating index of a given permutation

--- utils/math_utils.py
+++ utils/math_utils.py
@@ -63,7 +63,7 @@ class MathUtils:
             # Reduce the group size to match the limited space.
             # Now we are grouping by the next digit, since the current digit is already known.
             if group_size > 1:
-                group_size /= (perm_size - len(perm))
+                group_size //= (perm_size - len(perm))
             # When we reach a group size of 1 we cannot limit no more, we did just put the second-to-last element.
             # So we break out of the cycle and there's one more element left.
             else:
@@ -75,3 +75,56 @@ class MathUtils:
                 perm.append(elem)
                 break
         return perm
+
+    # Returns the lexicographical index of the given permutation.
+    # The given permutation is expected to be a list of the numbers 0,1,..,N in some order.
+    # Example: For size 3 the permutations in lexicographical order are:
+    #     012, 021, 102, 120, 201, 210
+    # The 0-th permutation with size 3 is [0, 1, 2] and the 5-th one is [2, 1, 0].
+    # So for example get_index_from_perm([1, 0, 2]) should return 2.
+    def get_index_from_perm(perm):
+        # The algorithm will be the following:
+        # For each element in the permutation, starting from the 0-th one to the last one,
+        # we group permutations based on their element at that index.
+        # We consider the elements that we already passed to be fixed,
+        # and look only at permutations starting with those exact elements.
+        # The value of each element can tell us in which group the permutation is,
+        # but we don't use the value directly, instead we need to count how many
+        # of the smaller elements are not already used, because they will be inside the non-fixed part.
+        # After finding the group index we multiply it by the size of the group
+        # to get the total number of permutations that are inside previous groups,
+        # and hence in previous permutations, so they need to be counted by the lexicographical index.
+        # Counting permutations of all such groups until the last element, when there is only one group,
+        # gives us the final index.
+        perm_size = len(perm)
+        # This will be the resulting lexicographical index.
+        lex_index = 0
+        # Current element's index
+        elem_index = 0
+        # Current group's size when grouping with the current element
+        group_size = MathUtils.factorial(perm_size - 1)
+        # Marking used elements. In the beginning no element is yet used, so fill with False.
+        used = [False] * perm_size
+        # Traverse elements until the second-to-last one.
+        # The last one will give us only a single group and will contribute 0 to the index.
+        while elem_index + 1 < perm_size:
+            elem = perm[elem_index]
+            # Find the group in which the current element is
+            # by counting how many smaller elements are unused
+            group_index = 0
+            for smaller_elem in range(elem):
+                if not used[smaller_elem]:
+                    group_index += 1
+            # Multiplying the group index by the size of the group
+            # gives us the number of permutations inside previous groups.
+            # We need to count those with the lex index.
+            lex_index += group_size * group_index
+            # Decrease the group size for the next element in the traversal
+            group_size //= (perm_size - 1 - elem_index)
+            # Mark the element as used
+            used[elem] = True
+            # Next element
+            elem_index += 1
+        # At this point permutations from all previous groups are counted,
+        # and these are all previous permutations, which gives us exactly the lexicographical index.
+        return lex_index
