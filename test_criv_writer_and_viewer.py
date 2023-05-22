ee253121f0003e25b1d7f5f2f37537b2ca665519
RubikRun <borisd2000@gmail.com>
2023-05-22 23:53:35 +0300

    add to test: correct data for hierarchical pixel permutation

--- test_criv_writer_and_viewer.py
+++ test_criv_writer_and_viewer.py
@@ -8,34 +8,47 @@ os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
 import pygame
 pygame.init()
 
-criv_expected_size = 120612940
-image_name = 'hires00'
+image_name = 'lores01'
 og_image_path = 'original_images/' + image_name + '.png'
 criv_image_path = 'encrypted_images/' + image_name + '.criv'
 
-# Get permutation and regions for permuting the image in the .criv file
-rgn_count = [24,3]
-perm_size = rgn_count[0] * rgn_count[1]
-perm = MathUtils.get_perm_from_index(
-    perm_size,
-    randint(0, MathUtils.factorial(perm_size) - 1)
-)
-inv_perm = MathUtils.get_inverse_perm(perm)
+# Create permutation and regions for permuting the image in the .criv file
+rgn_counts = [
+    [24,24],
+    [12,12],
+    [3,3]
+]
+perm_sizes = []
+perms = []
+inv_perms = []
+for i in range(len(rgn_counts)):
+    perm_sizes.append(rgn_counts[i][0] * rgn_counts[i][1])
+    perms.append(
+        MathUtils.get_perm_from_index(
+            perm_sizes[i],
+            randint(1, MathUtils.factorial(perm_sizes[i]) - 1)
+        )
+    )
+    print("Permutation at level " + str(i) + " is ", end='')
+    print(perms[i])
+    inv_perms.append(MathUtils.get_inverse_perm(perms[i]))
 
 # Write the .criv file
 CrivWriter.write_criv(
     og_image_path,
     criv_image_path,
-    perm,
-    rgn_count
+    perms,
+    rgn_counts
 )
 
 # View the .criv file
 CrivViewer.view_criv(
     criv_image_path,
-    inv_perm,
-    rgn_count,
-    [int(1920 / 2), int(1080 / 2)]
+    inv_perms,
+    rgn_counts,
+    # screen is 1366 by 768 
+    # image is 1008 by 1428
+    [int(1366 / 1.5), int(768 / 1.5)]
 )
 
 pygame.quit()
